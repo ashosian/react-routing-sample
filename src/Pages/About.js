84a82ba5d89c11166c6bcd8be977c9b51c374411
ashosian <ashok.rai13254@gmail.com>
2023-06-14 17:23:03 +0545

    changes

--- src/Pages/About.js
+++ src/Pages/About.js
@@ -3,6 +3,7 @@ import React from 'react'
 const About = () => {
   return (
     <div>
+      <h1>about</h1>
       <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Inventore laboriosam vero rerum unde recusandae eos delectus ad? Quae distinctio omnis mollitia architecto, laborum vitae repellat laudantium, vero eligendi facilis natus.</p>
     </div>
   )
